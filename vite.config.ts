c0eb909a09c31aa4206a95e18132f51ec7ddb578
Stadav01 <stanley.ndlovu@ashesi.edu.gh>
2025-01-07 16:38:45 +0000

    Final commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 export default defineConfig({
     plugins: [react()],
-    base: './',
+    base: '/', // Use root-relative paths for production
     optimizeDeps: {
         exclude: ['lucide-react'],
     },
